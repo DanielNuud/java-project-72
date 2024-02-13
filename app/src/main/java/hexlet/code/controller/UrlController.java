0983f48486af6a3341ac28060ab60bde1de158bf
DanielNuud <daaniel.nuud@gmail.com>
2024-02-13 11:45:18 +0200

    update UrlController

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -1,14 +1,26 @@
 package hexlet.code.controller;
 
+import hexlet.code.UrlCheck;
 import hexlet.code.dto.urls.UrlPage;
 import hexlet.code.dto.urls.UrlsPage;
 import hexlet.code.model.Url;
+import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
+
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.net.URI;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.net.URISyntaxException;
 import java.sql.SQLException;
@@ -22,6 +34,7 @@ public class UrlController {
         Matcher matcher = pattern.matcher(url);
         return matcher.matches();
     }
+
     public static void index(Context ctx) throws SQLException {
         var urls = UrlRepository.getEntities();
         var page = new UrlsPage(urls);
@@ -62,6 +75,10 @@ public class UrlController {
             ctx.sessionAttribute("flash", "Invalid URL");
             ctx.sessionAttribute("flash-type", "alert alert-danger");
             ctx.redirect("/");
+        } catch (SQLException e) {
+            ctx.sessionAttribute("flash", "URL is already exists!");
+            ctx.sessionAttribute("flash-type", "alert alert-danger");
+            ctx.redirect("/");
         }
     }
 
@@ -71,11 +88,50 @@ public class UrlController {
         var url = UrlRepository.find(id)
                 .orElseThrow(() -> new NotFoundResponse("Url with id = " + id + " not found"));
 
-        var page = new UrlPage(url);
+        var urlCheck = UrlCheckRepository.findByUrlId(id);
 
+        var page = new UrlPage(url, urlCheck);
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
 
         ctx.render("urls/show.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void createCheck(Context ctx) throws SQLException, IOException {
+        Long urlId = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        java.util.Date date = new java.util.Date();
+        long t = date.getTime();
+        java.sql.Timestamp sqlTimestamp = new java.sql.Timestamp(t);
+
+        try {
+            var url = UrlRepository.find(urlId);
+            Url newUrl = url.orElseThrow(() -> new RuntimeException("URL not found"));
+
+            HttpResponse<String> response = Unirest.get(newUrl.getName().trim())
+                    .asString();
+            int statusCode = response.getStatus();
+
+            Connection connection = Jsoup.connect(newUrl.getName());
+            Document document = connection.get();
+
+            String title = document.title();
+
+            Element h1Element = document.selectFirst("h1");
+            String h1 = (h1Element != null) ? h1Element.text() : "";
+
+            Element descriptionElement = document.selectFirst("meta[name=description]");
+            String description = (descriptionElement != null) ? descriptionElement.attr("content") : "";
+
+            var urlCheck = new UrlCheck(statusCode, title, h1, description, sqlTimestamp);
+            UrlCheckRepository.save(urlCheck);
+            ctx.sessionAttribute("flash", "URL is checked successfully!");
+            ctx.sessionAttribute("flash-type", "alert alert-success");
+            ctx.redirect("/urls/" + urlId);
+        } catch (Exception e) {
+            ctx.sessionAttribute("flash", "Error during URL check: " + e.getMessage());
+            ctx.sessionAttribute("flash-type", "alert alert-danger");
+            ctx.redirect("/urls/{id}");
+        }
     }
 }
