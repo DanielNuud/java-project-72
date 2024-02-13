a7b8593fdc532691f9bcf5a6a865ea1dfdd3d6f7
DanielNuud <daaniel.nuud@gmail.com>
2024-02-13 11:45:04 +0200

    update UrlPage

--- app/src/main/java/hexlet/code/dto/urls/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlPage.java
@@ -1,12 +1,17 @@
 package hexlet.code.dto.urls;
 
+import hexlet.code.UrlCheck;
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
+
 @AllArgsConstructor
 @Getter
 public class UrlPage extends BasePage {
     private Url url;
+    private List<UrlCheck> urlCheckList;
 }
