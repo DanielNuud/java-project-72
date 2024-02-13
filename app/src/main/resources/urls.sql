315172ed55b1bcbbbe9f94da5fa2385e0ee88935
DanielNuud <daaniel.nuud@gmail.com>
2024-02-13 11:44:38 +0200

    update urls

--- app/src/main/resources/urls.sql
+++ app/src/main/resources/urls.sql
@@ -2,6 +2,19 @@ DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
     createdAt TIMESTAMP NOT NULL
-)
+);
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    status_code INT,
+    title VARCHAR(255),
+    h1 VARCHAR(255),
+    description TEXT,
+    url_id BIGINT,
+    created_at TIMESTAMP,
+    FOREIGN KEY (url_id) REFERENCES urls (id)
+);
