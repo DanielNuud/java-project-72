1cd68e0d156259b1af422074d7049c8319a053cd
DanielNuud <daaniel.nuud@gmail.com>
2024-02-20 17:17:00 +0200

    some fixes

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
