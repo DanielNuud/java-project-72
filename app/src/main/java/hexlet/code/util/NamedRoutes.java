8bb12f355a9ef9f462811cb404006ce44756a40c
DanielNuud <daaniel.nuud@gmail.com>
2024-02-26 10:06:35 +0200

    update NamedRoutes

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -1,26 +1,32 @@
 package hexlet.code.util;
 
-public class NamedRoutes {
+public final class NamedRoutes {
+
     public static String rootPath() {
         return "/";
     }
+
+    public static String urlPath(Long id) {
+        return urlPath(String.valueOf(id));
+    }
+
+    public static String urlPath(String id) {
+        return "/urls/" + id;
+    }
+
     public static String urlsPath() {
         return "/urls";
     }
 
-    public static String urlPath(Long id) {
-        return "/urls/" + String.valueOf(id);
-    }
-
-    public static String urlPath() {
-        return "/urls/{id}";
-    }
-
-    public static String urlCheckPath() {
-        return urlPath() + "/check";
+    public static String urlsPath(int page) {
+        return "/urls?page=" + page;
     }
 
     public static String urlCheckPath(Long id) {
-        return urlPath(id) + "/check";
+        return urlCheckPath(String.valueOf(id));
+    }
+
+    public static String urlCheckPath(String id) {
+        return "/urls/" + id + "/checks";
     }
 }
