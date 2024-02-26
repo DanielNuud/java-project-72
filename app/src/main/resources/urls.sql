ffea5a838b76fc2439a5e163641e25de6d542217
DanielNuud <daaniel.nuud@gmail.com>
2024-02-26 10:08:25 +0200

    update urls.sql

--- app/src/main/resources/urls.sql
+++ app/src/main/resources/urls.sql
@@ -1,20 +1,22 @@
---DELETE FROM url_checks;
+DROP TABLE IF EXISTS urls CASCADE;
+
+CREATE TABLE urls
+(
+    id         SERIAL,
+    name       VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL,
+    CONSTRAINT pk_urls PRIMARY KEY (id)
+);
+
 DROP TABLE IF EXISTS url_checks;
-DROP TABLE IF EXISTS urls;
-
-CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    createdAt TIMESTAMP NOT NULL
-);
-
-CREATE TABLE url_checks (
-    id SERIAL PRIMARY KEY,
-    status_code INT,
-    title VARCHAR(255),
-    h1 VARCHAR(255),
+CREATE TABLE url_checks
+(
+    id          SERIAL,
+    url_id      BIGINT REFERENCES urls (id) NOT NULL,
+    status_code BIGINT,
+    h1          VARCHAR(255),
+    title       VARCHAR(255),
     description TEXT,
-    url_id BIGINT,
-    created_at TIMESTAMP,
-    FOREIGN KEY (url_id) REFERENCES urls (id)
-);
+    created_at  TIMESTAMP                   NOT NULL,
+    CONSTRAINT pk_url_checks PRIMARY KEY (id)
+)
