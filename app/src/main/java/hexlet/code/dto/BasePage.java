f191763be9d2d6d8116bb21cfc9b036cd5c0889e
DanielNuud <daaniel.nuud@gmail.com>
2024-02-26 10:05:48 +0200

    update BasePage

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -1,11 +1,14 @@
 package hexlet.code.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class BasePage {
-    private String flash;
-    private String flashType;
+    private String message;
 }
