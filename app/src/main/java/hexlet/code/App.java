306e745bef411018523c01d921f4674f9c5265b8
DanielNuud <daaniel.nuud@gmail.com>
2024-02-26 10:05:36 +0200

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,63 +1,68 @@
 package hexlet.code;
 
-import hexlet.code.controller.RootController;
-import hexlet.code.controller.UrlController;
-import hexlet.code.repository.BaseRepository;
-import hexlet.code.util.NamedRoutes;
-import io.javalin.Javalin;
-import lombok.extern.slf4j.Slf4j;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
-import io.javalin.rendering.template.JavalinJte;
 import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.controller.UrlChecksController;
+import hexlet.code.controller.UrlController;
+import hexlet.code.repository.BaseRepository;
+import hexlet.code.util.NamedRoutes;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
 @Slf4j
-public final class App {
+public class App {
+
+    private static String getDatabaseUrl() {
+        return System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:webapp;DB_CLOSE_DELAY=-1;");
+    }
+
+    private static InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = App.class.getClassLoader();
+        return classLoader.getResourceAsStream(fileName);
+    }
+
+    private static String getContentFromStream(InputStream is) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
     private static TemplateEngine createTemplateEngine() {
         ClassLoader classLoader = App.class.getClassLoader();
         ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
         TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
         return templateEngine;
     }
+
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.valueOf(port);
-    }
-
-    private static String readResourceFile(String fileName) throws IOException {
-        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
-        if (inputStream == null) {
-            throw new IOException("File not found: " + fileName);
-        }
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-            return reader.lines().collect(Collectors.joining("\n"));
-        }
-    }
-
-    private static HikariDataSource createDataSource() throws SQLException {
-        String jdbcUrl = System.getenv()
-                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
-
-        var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(jdbcUrl);
-
-        return new HikariDataSource(hikariConfig);
+        return Integer.parseInt(port);
     }
 
     public static Javalin getApp() throws IOException, SQLException {
-        JavalinJte.init(createTemplateEngine());
 
-        var dataSource = createDataSource();
-        var sql = readResourceFile("urls.sql");
+        var hikariConfig = new HikariConfig();
+        String databaseUrl = getDatabaseUrl();
+        if (databaseUrl.contains("postgresql")) {
+            hikariConfig.setDriverClassName(org.postgresql.Driver.class.getName());
+        }
+        hikariConfig.setJdbcUrl(databaseUrl);
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        String sql = getContentFromStream(getFileFromResourceAsStream("urls.sql"));
 
         log.info(sql);
         try (var connection = dataSource.getConnection();
@@ -70,17 +75,22 @@ public final class App {
             config.plugins.enableDevLogging();
         });
 
+        JavalinJte.init(createTemplateEngine());
+
+        app.before(ctx -> ctx.contentType("text/html; charset=utf-8"));
+
+        app.get(NamedRoutes.rootPath(), UrlController::build);
         app.post(NamedRoutes.urlsPath(), UrlController::create);
-        app.get(NamedRoutes.rootPath(), RootController::index);
         app.get(NamedRoutes.urlsPath(), UrlController::index);
-        app.get(NamedRoutes.urlPath(), UrlController::showUrl);
-        app.post(NamedRoutes.urlCheckPath(), UrlController::createCheck);
+        app.get(NamedRoutes.urlPath("{id}"), UrlController::show);
+        app.post(NamedRoutes.urlCheckPath("{id}"), UrlChecksController::check);
 
         return app;
     }
 
-    public static void main(String[] args) throws SQLException, IOException {
-        Javalin app = getApp();
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+
         app.start(getPort());
     }
 }
