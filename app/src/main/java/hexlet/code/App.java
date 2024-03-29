65db5380756bcc39a8800cf9b5473e0776459fdd
DanielNuud <daaniel.nuud@gmail.com>
2024-03-29 11:53:34 +0200

    fix linter errors

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,9 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
